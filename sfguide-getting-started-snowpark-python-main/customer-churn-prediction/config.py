56d745eb66a6c32e9d9652dad2fadfd4e11aa16c
amita22garg <113573956+amita22garg@users.noreply.github.com>
2022-09-15 11:14:06 +0530

    Add files via upload

---
+++ sfguide-getting-started-snowpark-python-main/customer-churn-prediction/config.py
@@ -0,0 +1,13 @@
+snowflake_conn_prop = {
+   "account": "og26311.ap-south-1.aws",
+   "user": "amitagarg22",
+   "password": "Sattguru@1",
+   "role": "ACCOUNTADMIN",
+   "database": "snowpark_quickstart",
+   "schema": "TELCO",
+   "warehouse": "sp_qs_wh",
+}
+
+# Snowflake Account Identifiers
+# https://docs.snowflake.com/en/user-guide/admin-account-identifier.html#account-identifier-formats-by-cloud-platform-and-region
+#  
